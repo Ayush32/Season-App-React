d462cb7ed031cd2627a6ffa4df044a0d98c344dc
Ayush Gupta <721ayush@gmail.com>
2020-08-11 20:06:06 +0530

    setting geolocation api

--- src/SeasonDIsplay.js
+++ src/SeasonDIsplay.js
@@ -2,3 +2,12 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
+
+import React from "react";
+
+const SeasonDisplay = () => {;
+ return <div>Season Display</div>
+}
+
+
+ exports default SeasonDisplay
