e1f5f16fcef9bdb046de6e22d17cec1949ea549b
Ayush Gupta <721ayush@gmail.com>
2020-08-13 07:49:40 +0530

    fetch laltitude

--- src/index.js
+++ src/index.js
@@ -9,17 +9,21 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
+    // this is the only time we do directly assignment
+    // to this.state
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
