651b580e73821829f69719524b4cf5b2cd05f975
Ayush Gupta <721ayush@gmail.com>
2020-08-29 13:52:26 +0530

    component

--- src/SeasonDIsplay.js
+++ src/SeasonDIsplay.js
@@ -4,8 +4,21 @@
  */
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+
+  return <div>{text}</div>;
 };
 
 export default SeasonDisplay;
