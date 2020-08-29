d6ec7f8a2d7bf6060c7f5785ef34fc604c365e63
Ayush Gupta <721ayush@gmail.com>
2020-08-29 06:54:04 +0530

    making aseson app

--- src/SeasonDIsplay.js
+++ src/SeasonDIsplay.js
@@ -2,12 +2,10 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
-
 import React from "react";
 
-const SeasonDisplay = () => {;
- return <div>Season Display</div>
-}
+const SeasonDisplay = () => {
+  return <div>Season Display</div>;
+};
 
-
- exports default SeasonDisplay
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -6,33 +6,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // this is the only time we do directly assignment
-    // to this.state
-    this.state = { lat: null, errorMessage: "" };
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
+// scaffolding APP
 
-  // React says we have to define render!!
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude {this.state.lat}</div>;
-    }
-    return <div>Loading......</div>;
-  }
-}
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
