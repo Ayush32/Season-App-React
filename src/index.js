756eaeeee961d4a93308953afadb42e8fb46fd23
Ayush Gupta <721ayush@gmail.com>
2020-08-29 07:21:51 +0530

    state system

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ import ReactDOM from "react-dom";
 
 // scaffolding APP
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.components {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
